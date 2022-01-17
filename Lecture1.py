f9e163eb14ddf0187f8bc58abddc5caf2631392d
suyeon1109 <97889490+suyeon1109@users.noreply.github.com>
2022-01-17 22:02:05 +0900

    Create Lecture1.py

---
+++ Lecture1.py
@@ -0,0 +1,61 @@
+#변수 -> variable
+
+#정수 = integer
+x=1
+y=2
+z=3
+
+
+#문자열= string
+tablet = "iPad"
+phone = "Iphone"
+labtop = "MacBook"
+appleLabtop = "IMac"
+
+#소수 = float or double
+a = 0.1
+b = 1.1
+c = 1.23245
+
+print(x/y)
+
+# print(tablet)
+# print(labtop)
+# print(appleLaptop)
+
+print(tablet, phone, labtop, appleLabtop)
+suyeon = "I"
+verb = "Love"
+noun = "NCT"
+
+print(suyeon, verb, noun)
+
+suyeon = "You"
+verb = "love"
+noun = "MonstaX"
+print(suyeon, verb, noun)
+
+suyeon = "She"
+verb = "Likes"
+noun = "suyeon"
+print(suyeon, verb, noun)
+
+#반복문
+num = 0
+while num < 3:
+    print(suyeon, verb, noun)
+    num = num + 1
+print("while 문이 끝났습니다")
+print("num:", num)
+
+suyeon = "He"
+verb = "Likes"
+noun = "Suyeon"
+
+num = 0
+while true:
+    print(suyeon, verb, noun)
+    num = num + 1
+    print(num)
+print("while문이 끝났습니다")
+print("num:", num)
