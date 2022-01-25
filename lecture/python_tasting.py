5f8d069e13ad3c1c3c823c3b699a8477f889cfce
suyeon1109 <97889490+suyeon1109@users.noreply.github.com>
2022-01-26 08:00:48 +0900

    data-type lecture

---
+++ lecture/python_tasting.py
@@ -0,0 +1,92 @@
+# #변수 -> variable
+
+# #정수 = integer
+# x=1
+# y=2
+# z=3
+
+
+# #문자열= string
+# tablet = "iPad"
+# phone = "Iphone"
+# labtop = "MacBook"
+# appleLabtop = "IMac"
+
+# #소수 = float or double
+# a = 0.1
+# b = 1.1
+# c = 1.23245
+
+# print(x/y)
+
+# # print(tablet)
+# # print(labtop)
+# # print(appleLaptop)
+
+# print(tablet, phone, labtop, appleLabtop)
+# suyeon = "I"
+# verb = "Love"
+# noun = "NCT"
+
+# print(suyeon, verb, noun)
+
+# suyeon = "You"
+# verb = "love"
+# noun = "MonstaX"
+# print(suyeon, verb, noun)
+
+# suyeon = "She"
+# verb = "Likes"
+# noun = "suyeon"
+# print(suyeon, verb, noun)
+
+# #반복문
+# num = 0
+# while num < 3:
+#     print(suyeon, verb, noun)
+#     num = num + 1
+# print("while 문이 끝났습니다")
+# print("num:", num)
+
+# suyeon = "He"
+# verb = "Likes"
+# noun = "Suyeon"
+
+# num = 0
+# while true:
+#     print(suyeon, verb, noun)
+#      num = num + 1
+#      print(num)
+# print("while문이 끝났습니다")
+# print("num:", num)
+
+#사칙연산
+print( 1 + 2 )
+print( 3 * 9 )
+a=1
+b=2
+print(a + b)
+#변수에 문자대입
+a= "python"
+print(a)
+
+#조건문
+a = 3
+if a > 1:
+    print("a is greater than 1")
+
+#반복문 for
+for a in [1,2,3]:
+    print(a)
+
+#반복문 while
+i = 0
+while i<3:
+    i= i + 1
+    print(i)
+
+#함수
+def add(a,b):
+    return a+b
+
+print(add(3,4))
--- suyeon.py
+++ suyeon.py
@@ -1,2 +1,26 @@
 print("I love NCT")
 print("I love MonstaX")
+
+print("-------------------------")
+
+num = 10
+while num>0:
+    print(num)
+    print("above0")
+    num = num - 1
+print(num)
+print("finally zero")
+
+print("-------------------------")
+
+a = 1
+b = 1
+while a<10:
+    b = 1
+    while b<10:
+        print(a*b)
+        b = b+1
+        print("idnyl")
+    a = a+1
+    print("complete")
+print("Full Table")
