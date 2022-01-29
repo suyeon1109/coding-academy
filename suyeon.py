e57faf972d75a0f68813f3aedae2f54242415d74
suyeon1109 <97889490+suyeon1109@users.noreply.github.com>
2022-01-29 22:51:32 +0900

    Update suyeon.py

--- suyeon.py
+++ suyeon.py
@@ -51,3 +51,107 @@ print(generalAdd(10,9,8,7,6,5,4,3,2,1))
 print(generalAdd(3,3,3,3,3,3))#중복 허용
 
 print("------------------------------")
+
+#infinite loop
+#while 1:
+    #print("infinite loop")
+#while True:
+    #print("This never ends") #이거는 안나옴 무슨짓을 해도 왜냐면 위에꺼가 무한대로 나와서
+
+#break-무슨 일이 있던 간에 반복문 탈출
+val1=10
+while val1>0:
+    print(val1)
+    if val1==3:
+        break
+    val1-=1
+print("outside the loop")
+print(val1)
+
+print("---------------------------")
+
+
+#continue - 조건 확인 절차로 다시 넘어감
+limit=100
+totalsum=0
+while limit>0:
+    if limit%3==0:   #%는 나머지
+        totalsum+=limit
+        limit-=1
+    else:
+        limit-=1
+        continue
+print(totalsum)  #100이하의 3의 배수의 총합을 구하는 것
+print("---------------------------")
+
+
+'''501부터 1000까지 2와 5의 배수의 합을 구하기
+result: 225250'''
+
+limit=1000
+totalsum=0
+while 501<=limit<=1000:
+    if limit%2==0:   #%는 나머지
+        totalsum+=limit
+        limit-=1
+    else:
+        limit-=1
+        continue
+
+limit=1000
+totaladdition=0
+while 501<=limit<=1000:
+    if limit%5==0:   #%는 나머지
+        totalsum+=limit
+        limit-=1
+    else:
+        limit-=1
+        continue
+
+limit=1000
+toTalsum=0
+while 501<=limit<=1000:
+    if limit%10==0:
+        toTalsum+=limit
+        limit-=1
+    else:
+        limit-=1
+        continue
+    
+print(totalsum+totaladdition-toTalsum)
+
+print("---------------------------")
+print("jdhnvkdsmdclokfmv")
+
+limit=1000
+result=0
+while limit>500:
+    if limit%2==0:
+        result+=limit
+    if limit%5==0:
+        result+=limit
+    if limit%10==0:
+        result-=limit
+    limit-=1
+print(result)
+#while 한번만 쓰고 풀기
+print("---------------------------")
+print("ajsncskdmco")
+
+#for
+#for 변수 in range/ for 변수 in 리스트
+for i in range(1,11,2):  #range(a,b,c): a부터 b-1까지의 값을 c의 간격으로 반복해주는 함수
+    print(i*i)                   #여기서는 1 3 5 7 9
+
+for i in range(1,10):
+    for j in range(1,10):    #c가 1일ㄸㅐ는 생략가능
+        print(i*j)
+        print("LOL")
+    print("complete")
+print("Full table")
+print("---------------------------")
+
+name=["Kim","Lee","Park","Choi","Jeong"]
+for k in name:    #k는 name이라는 list안에 있는 각 element들이 들어감
+    print((name.index(k)+1)*k)   #index는 그 element 의 index (몇번째인지)를 나타냄
+
